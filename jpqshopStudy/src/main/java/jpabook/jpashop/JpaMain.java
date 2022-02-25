525b8257ad7db949bffca5c1adca4615457603f6
JiHyun Jang <gihyun0818@gmail.com>
2022-02-25 17:04:02 +0900

    연관관계 매핑 기초 완료

--- jpqshopStudy/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpqshopStudy/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop;
 
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -15,7 +17,8 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin(); // 시작
         try{
-
+            Order order = new Order();
+            order.addOrderItem(new OrderItem());
             tx.commit(); //커밋 --이 순간 insert sql을 db에 보낸다.
         }catch (Exception e){
             tx.rollback() ;
